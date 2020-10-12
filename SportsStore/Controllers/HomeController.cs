4603ace91affc5536ca99cdab816c92162a763d3
Christian Jung <Jan.Junius@outlook.de>
2020-10-12 10:54:20 +0200

    Add project files.

---
+++ SportsStore/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        // In Startup wurde MapDefaultControllerRoute hinzugefügt: Hierüber wird gesteuert, wie ASP.NET Core URLs zu Controller-Klassen matched
+        // Der Default besagt dabei, dass die Index Methode des HomeControllers für alle URLs aufgerufen wird
+        // Die Index-Methode ruft dann die von Cotroller geerbte View-Methode auf, welche dafür sorgt, dass eine View mit dem Namen Index im Home-Ordner
+        // der Views aufgerufen wird.
+        public IActionResult Index() => View();
+    }
+}
---
+++ SportsStore/Models/IStoreRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsStore.Models
+{
+    public interface IStoreRepository
+    {
+        // IQueryable ist ähnlich wie IEnumerable, allerdings optimiert für die Datenabfrage und daher sinnvoll im Context des EF
+        // Wichtiger Unterschied zu IEnumerable: Mit jeder Drüberlaufen über die Enumeration wird ein neues SQL an die DB geschickt!
+        // Um dies zu verhindern, muss die Enumeration erst in eine Liste (.ToList()) umgewandelt werden
+        IQueryable<Product> Products { get; }
+    }
+}
